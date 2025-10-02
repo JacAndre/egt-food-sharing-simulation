77e314eb73af08e551fa14635c17e934891c7685
Jacques Andre <jacques_andre@ymail.com>
2025-10-02 15:14:02 +0100

    Updating Simulation class to utilise GridManager for placing Agents

--- src/main/java/com/jacandre/Simulation.java
+++ src/main/java/com/jacandre/Simulation.java
@@ -26,38 +26,26 @@ public class Simulation {
         log.info("Simulation initialized with {} agents on a {}x{} grid.", livingAgents.size(), Constants.GRID_SIZE, Constants.GRID_SIZE);
     }
 
-    private List<Point> initialisePositions() {
-        List<Point> availablePositions = new ArrayList<>();
-        for (int x = 0; x < Constants.GRID_SIZE; x++) {
-            for (int y = 0; y < Constants.GRID_SIZE; y++) {
-                availablePositions.add(new Point(x, y));
-            }
-        }
-
-        Collections.shuffle(availablePositions, Constants.RANDOM);
-        return availablePositions;
-    }
-
     private void initialiseAgents() {
-        // All possible positions
-        List<Point> availablePositions = new ArrayList<>();
-        for(int i = 0; i < Constants.GRID_SIZE; i++) {
-            for(int j = 0; j < Constants.GRID_SIZE; j++) {
-                availablePositions.add(new Point(j, i));
+        for (int i = 0; i < Constants.NUM_AGENTS; i++) {
+            Point pos = gridManager.getNextAvailablePosition();
+            if (pos == null) {
+                log.warn("Ran out of space while placing agents. {}/{} agents placed.", livingAgents.size(), Constants.NUM_AGENTS);
+                break;
             }
-        }
 
-        Collections.shuffle(availablePositions);
-
-        for (int i = 0; i < Constants.NUM_AGENTS && i < availablePositions.size(); i++) {
-            Point pos = availablePositions.get(i);
             Strategy strategy = Constants.RANDOM.nextBoolean() ? Strategy.HELPER : Strategy.SELFISH;
             Agent agent = new Agent(strategy);
-            gridManager.placeEntity(agent, pos);
-            livingAgents.add(agent);
+
+            boolean placed = gridManager.placeEntity(agent, pos);
+            if (placed) {
+                livingAgents.add(agent);
+            } else {
+                log.warn("Failed to place agent at {}", pos);
+            }
         }
 
-        log.info("{} Agents populated to Grid", livingAgents.size());
+        log.info("{} agents populated to grid.", livingAgents.size());
     }
 
     private void generateFoodSource() {
