e37ed1c8d5484013c6f67daa9e95eab6ec0dc519
Jacques Andre <jacques_andre@ymail.com>
2025-10-03 08:50:04 +0100

    Updated constants

--- src/main/java/com/jacandre/models/Constants.java
+++ src/main/java/com/jacandre/models/Constants.java
@@ -7,19 +7,19 @@ public class Constants {
     public static final int GRID_SIZE = 50;
     public static int NUM_AGENTS = 1500; // Total number of agents (max GRID_SIZE * GRID_SIZE)
     public static final int MAX_FOOD_SOURCES = 5;
-    public static final double FOOD_SPAWN_PROBABILITY = 0.2; // 20% chance per tick
+    public static final double FOOD_SPAWN_PROBABILITY = 0.35; // 20% chance per tick
 
     // Core Game Parameters
     public static final double INITIAL_ENERGY = 100.0;
-    public static final double REPRODUCTION_THRESHOLD = 30.0; // Fitness level to trigger reproduction
-    public static final int REPRODUCTION_COOLDOWN = 25;
-    public static final double COST_OF_LIVING = 0.5; // Includes movement cost
-    public static final double MOVE_COST = 1; // Includes movement cost
-    public static final double FOOD_REWARD = 20.0;
+    public static final double REPRODUCTION_THRESHOLD = 50.0; // Fitness level to trigger reproduction
+    public static final int REPRODUCTION_COOLDOWN = 40;
+    public static final double COST_OF_LIVING = 0.4; // Includes movement cost
+    public static final double MOVE_COST = 0.7; // Includes movement cost
+    public static final double FOOD_REWARD = 25.0;
     public static final int FOOD_LIFESPAN = 100;
 
     // Assist Mechanism Parameters
-    public static final double ASSIST_COST = 2.0; // Cost incurred by Helper when offering assistance
+    public static final double ASSIST_COST = 1.0; // Cost incurred by Helper when offering assistance
     public static final double SHARE_RATE = 0.2; // Percentage of FOOD_REWARD the Helper demands
     public static final double LOW_ENERGY_THRESHOLD = 5.0; // Fitness level to trigger an assist
 
